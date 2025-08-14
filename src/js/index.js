01e292802d889370c0b8155b16f80819e8f782e8
SergeevaEA <ekatandr002@mail.ru>
2025-08-14 15:33:57 +0300

    Step 1 v13.2 Bootstrap

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,5 @@
-console.log('Hello')
+// Import our custom CSS
+import '../css/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import path from 'path'
 
 export default {
-  root: path.resolve(__dirname, 'src'),
+  root: '.',
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true, // отключает Deprecation Warning из node_modules
+      },
     },
   },
   server: {
