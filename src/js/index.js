b97e146ed30b2e9a669cad309c6ff2b0177c3489
SergeevaEA <ekatandr002@mail.ru>
2025-08-19 16:25:07 +0300

    Step 2 v3 index.js

--- src/js/index.js
+++ src/js/index.js
@@ -6,32 +6,44 @@ import '../css/styles.scss'
 
 import * as yup from 'yup'
 import onChange from 'on-change'
-//const schema = yup.string()
 
-/*export default () => {
+const schema = yup.string().url('Ссылка должна быть валидным URL').required('Ссылка должна быть валидным URL')
+const isURLValid = (url, feeds) => {
+  return schema.validate(url)
+    .then(() => {
+      if (feeds.includes(url)) {
+        return Promise.reject(new yup.ValidationError('Такой URL уже существует'))
+      }
+    })
+    .catch(error => console.log(error.message))
+}
+
+export default () => {
   const elements = {
     form: document.querySelector('.rss-form'),
-    urlInput,
-    submitButton,
-  }
-}*/
-
-const initialState = {
-  feeds: [], // список новостных лент (добавленных URL)
-  process: {
-    processState: 'filling', // filling, sending, sent, error
-    processError: null, // ошибки при отправке
-  },
-  form: {
-    valid: true, // валидна ли форма
-    errors: {}, // ошибки валидации
     fields: {
-      url: '', // текстовое поле для URL
+      url: document.getElementById('url-input'),
     },
-    fieldsUI: {
-      touched: {
-        url: false, // был ли пользовательский ввод
+    submitButton: document.querySelector('input[type="submit"]'),
+  }
+
+  const initialState = {
+    feeds: [], // список новостных лент (добавленных URL)
+    process: {
+      processState: 'filling', // filling, sending, sent, error
+      processError: null, // ошибки при отправке
+    },
+    form: {
+      valid: true, // валидна ли форма
+      errors: {}, // ошибки валидации
+      fields: {
+        url: '', // текстовое поле для URL
+      },
+      fieldsUI: {
+        touched: {
+          url: false, // был ли пользовательский ввод
+        },
       },
     },
-  },
+  }
 }
